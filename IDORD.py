ab06abdf83a717c611a94318fee13b394af29464
Cyrus <aharonno2017@gmail.com>
2022-02-22 15:16:52 +0600

    reformed overal

---
+++ IDORD.py
@@ -0,0 +1,40 @@
+import os
+import subprocess
+
+
+class colors:
+    """Note you need to add  colors.ENDC every time you changed the color 
+       like this : 
+       print(colors.WARNING + "Warning" + colors.ENDC)
+    """
+    
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+x = subprocess.check_output(['scrapy', 'crawl', 'prothomalo'], cwd='idord_infograther/')
+
+print(f">>>>>>>>>>>>>>>>>{x}>>>>>>>>>>>>>>")
+
+print(colors.WARNING + "Warning: No active frommets remain. Continue?" + colors.ENDC)
+
+
+
+#Below this for Production 
+# from subprocess import Popen, PIPE
+
+
+# print("Step 1/X")
+# p = Popen(['scrapy', 'crawl', 'prothomalo'], stdout=PIPE, stderr = PIPE)
+# output = p.communicate()[0]
+# if p.returncode != 0: 
+#     print("> Setp Failed")
+# else:
+#     print("> Setp 1 Completed Successfully")
---
+++ README.md
@@ -0,0 +1,20 @@
+# IDROD
+
+## _The Only IDORD tools, Ever_
+
+[![Build Status](https://travis-ci.org/joemccann/dillinger.svg?branch=master)](https://travis-ci.org/joemccann/dillinger)
+
+## Installation
+
+IDORD requires Python and pip to run.
+
+Install the dependencies and start the tool.
+
+```sh
+pip install -r requirements.txt
+
+#Active the virtual env
+
+RUN: py IDORD.py 
+#bang bang
+```
